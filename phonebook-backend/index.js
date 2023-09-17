70dd1891fef09e02bf681e9a873c4eac487b6183
TheoSludge <amarko.c@hotmail.com>
2023-09-17 16:24:56 +0200

    3.11 done and minor tweaks

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -9,6 +9,7 @@ morgan.token('data', (request, response) => {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
 
